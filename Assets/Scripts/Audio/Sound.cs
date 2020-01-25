b1fc5d8f48532f5cac0db6e78422130a50cfecf6
Stefan Krsmanovic <krsma33@gmail.com>
2020-01-25 02:11:16 +0100

    Started adding sound scripts and assets

---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string Name;
+
+    public AudioClip Clip;
+
+    [Range(0f, 1f)]
+    public float Volume;
+    [Range(0.1f, 3f)]
+    public float Pitch;
+    public float StartSeconds;
+    public float EndSeconds;
+
+    [HideInInspector]
+    public AudioSource Source;
+
+    public void PlaySound()
+    {
+        if (EndSeconds <= 0)
+            EndSeconds = Clip.length;
+
+        Source.time = StartSeconds;
+        Source.Play();
+        Source.SetScheduledEndTime(AudioSettings.dspTime + (EndSeconds - StartSeconds));
+    }
+}
---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] Sounds;
+
+    void Awake()
+    {
+        foreach (var sound in Sounds)
+        {
+            sound.Source = gameObject.AddComponent<AudioSource>();
+            sound.Source.clip = sound.Clip;
+            sound.Source.volume = sound.Volume;
+            sound.Source.pitch = sound.Pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        var sound = Array.Find(Sounds, s => s.Name == name);
+        sound.PlaySound();
+    }
+
+
+}
